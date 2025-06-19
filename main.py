e1bd856c326fc2dc2d76c6fe724f63a9c893b5ba
TheCosmicSlug2 <solalvig@orange.fr>
2025-06-19 21:16:52 +0200

    code refractor

--- main.py
+++ main.py
@@ -1,172 +1,158 @@
-import random
-import pygame
+from random import shuffle
 from sys import exit as sysexit
+import pygame as pg
+from settings import nb_values, screend, line_width
 
-# Initialiser Pygame
-pygame.init()
+# Initialiser pg
+pg.init()
 
 # Configuration de la fenêtre
-width, height = 800, 600
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Sorting algorythms')
+screen = pg.display.set_mode(screend)
+pg.display.set_caption('Sorting algorythms')
 
 # Couleurs
-white = (255, 255, 255)
-black = (0, 0, 0)
-red = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 
 # Boucle principale
+clock = pg.time.Clock()
 
-distance = 1
-
-clock = pygame.time.Clock()
-
-fps = 60 # on se limite à 60 fps (max pour l'ordi)
+FPS = 0 # on se limite à 60 fps (max pour l'ordi)
 
 frame = 0
-running = True
 
-
-
-
+ratio = (screend[1] - 200) / nb_values
 
 
 def check_sorted(array):
-    for idx in range(len(array) - 1):
-        if array[idx] >= array[idx + 1]:
-            return False
-    return True
+    return all(a < b for a, b in zip(array, array[1:]))
 
+def update(array, current):
+    draw_lines(array, current)
+    check_events()
 
-def draw_line(x, width, height):
+tot_x = screend[0] - line_width * nb_values
+startx = tot_x // 2
+bottom_margin = 100
 
-    pygame.draw.rect(screen, white, (x, 500-height, width, height)) # x, y, width, longueur
+def draw_lines(array, current):
+    screen.fill(BLACK)
 
+    for idx, nb in enumerate(array):
+        color = RED if idx == current else WHITE
+        new_nb = nb * ratio
+        x = startx + (idx * line_width)
+        y = screend[1] - (bottom_margin + new_nb)
+        rect = pg.Rect(x, y, line_width, new_nb)
+        pg.draw.rect(screen, color, rect)
 
+    pg.display.flip()
 
-def draw_lines(array):
-    screen.fill(black)
-
-
-    len_array = len(array)
-
-    # la width est à 800, mais on part pour 600 "d'utilisable" (100 inutilisables de chaque côté)
-
-    line_width = 600/len_array # on peut pas vraiment arrondir, sinon décalages
-
-    pygame.display.set_caption(f"{len_array} {line_width}")
-
-    for idx in range(len(array)):
-        x = 100 + (idx * line_width)
-        line_height = array[idx]
-        draw_line(x, line_width, line_height)
-
-
-
-    pygame.display.update()
-
-    clock.tick(90)
-
-
+    clock.tick(FPS)
 
 
 def bubble_sort(array):
-
+    pg.display.set_caption("Bubble sort")
     array_sorted = False
 
-    while array_sorted == False:
+    while not array_sorted:
 
         n = len(array)
         for i in range(n - 1):
             for j in range(0, n - i - 1):
                 if array[j] > array[j + 1]:
                     array[j], array[j + 1] = array[j + 1], array[j]
-            draw_lines(array)
-            check_events()
+                update(array, j)
             array_sorted = check_sorted(array)
 
-    draw_lines(array)
+    update(array, i)
 
 def coktail_sort(array):
-
+    pg.display.set_caption("Cocktail Sort")
     array_sorted = False
 
-    while array_sorted == False:
+    while not array_sorted:
 
         n = len(array)
         swapped = True
         start = 0
         end = n - 1
-        while (swapped == True):
+        while swapped == True:
             swapped = False
 
             for i in range(start, end):
-                if (array[i] > array[i + 1]):
+                if array[i] > array[i + 1]:
                     array[i], array[i + 1] = array[i + 1], array[i]
                     swapped = True
+                update(array, i)
 
-            if (swapped == False):
+            if not swapped:
                 break
 
             swapped = False
             end -= 1
 
             for i in range(end - 1, start - 1, -1):
-                if (array[i] > array[i + 1]):
+                if array[i] > array[i + 1]:
                     array[i], array[i + 1] = array[i + 1], array[i]
                     swapped = True
+                update(array, i)
 
             start = start + 1
 
-            draw_lines(array)
-            check_events()
 
             array_sorted = check_sorted(array)
 
-    draw_lines(array)
+    update(array, start)
 
 
 def insertion_sort(array):
-
+    pg.display.set_caption("Insertion Sort")
     array_sorted = False
 
-    while array_sorted == False:
+    while not array_sorted:
 
         for idx in range(1, len(array)):
             value = array[idx]
             value2 = idx - 1
             while value2 >= 0 and value < array[value2]:
+                update(array, value2)
                 array[value2 + 1] = array[value2]
                 value2 -= 1
             array[value2 + 1] = value
 
-            draw_lines(array)
-            check_events()
             array_sorted = check_sorted(array)
 
-    draw_lines(array)
+    update(array, idx)
 
 def check_events():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
             sysexit()
 
-while running:
 
-    array = [_ for _ in range(400)]
+def run_with_profile():
+    array = list(range(0, nb_values + 1))
+    shuffle(array)
+    insertion_sort(array.copy())
 
-    random.shuffle(array)
-    insertion_sort(array)
+    shuffle(array)
+    bubble_sort(array.copy())
 
-    random.shuffle(array)
-    bubble_sort(array)
-
-    random.shuffle(array)
-    coktail_sort(array)
-
-    running = False
-
-pygame.quit()
+    shuffle(array)
+    coktail_sort(array.copy())
 
 
+if __name__ == "__main__":
+    # profiler = cProfile.Profile()
+    # profiler.enable()
 
+    run_with_profile()
+
+    # profiler.disable()
+    # profiler.dump_stats("profiling_results.prof")
+    # print("Profil enregistré dans 'profiling_results.prof'.")
+
+
+pg.quit()
---
+++ settings.py
@@ -0,0 +1,3 @@
+screend = (800, 600)
+nb_values = 100
+line_width = 5
